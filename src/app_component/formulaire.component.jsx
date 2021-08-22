4138726a8961ff8bd85c68814c78725f1b2d75e7
Olatundji ALY <alyolatundji22@gmail.com>
2021-08-22 13:10:28 +0200

    first commit

--- src/app_component/formulaire.component.jsx
+++ src/app_component/formulaire.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './formulaire.style.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init ( ) ;
 
 const Formulaire = props => {
 
@@ -9,10 +13,14 @@ const Formulaire = props => {
             <div>{ props.error ? error():null }</div>
             <form onSubmit={props.loadweather}>
                 <div className="row">
-                    <div className="col-md-3 offset-md-4">
+                    <div data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" className="col-md-3 offset-md-4">
                         <input type="text" className="form-control" name="city" autoComplete="off" placeholder="Entrer une ville"/>
                     </div>
-                    <div className="col-md-3 mt-md-0 text-md-left">
+                    <div data-aos="fade-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" className="col-md-3 mt-md-0 text-md-left">
                         <button className="btn btn-warning">Vois la Météo</button>
                     </div>
                 </div>
