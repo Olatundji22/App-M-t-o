57e371717413099316016dd87ad514fed9ef0a0c
Olatundji ALY <alyolatundji22@gmail.com>
2021-12-03 13:36:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends React.Component{
     const city = e.target.elements.city.value;
 
     if(city){
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
 
       const response = await api_call.json();
 
